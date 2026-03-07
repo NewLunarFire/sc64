0110d0807d8ee5106ebe13a38188eb0e19ef6bbd
Tommy Savaria <tommy.savaria@protonmail.ch>
2026-03-07 14:04:19 -0500

    Improve build process by adding decompressor and moving to new CRC tool

--- asm/setup.sh
+++ asm/setup.sh
@@ -8,12 +8,15 @@ wget https://github.com/ARM9/bass/releases/download/v18/bass-ubuntu.zip -nc -O .
 unzip -n .setup/bass-ubuntu.zip -d bass-ubuntu
 chmod +x bass-ubuntu/bass
 
-# Get N64 CRC tool
-wget https://dl.smwcentral.net/8799/rn64crc2.zip -nc -O .setup/rn64crc2.zip
-unzip -n .setup/rn64crc2.zip -d rn64crc
+# Get OOT Decompressor an N64 crc tool
+mkdir -p tools
+wget https://github.com/NewLunarFire/OoT_Decompressor/releases/download/sc64/decompressor -O tools/decompressor
+wget https://github.com/NewLunarFire/OoT_Decompressor/releases/download/sc64/n64crc -O tools/n64crc
+chmod +x tools/*
 
 # Get SC64 deployer
 mkdir -p sc64-deployer
 wget https://github.com/Polprzewodnikowy/SummerCart64/releases/download/v2.20.2/sc64-deployer-linux-v2.20.2.tar.gz -nc -O .setup/sc64-deployer-linux-v2.20.2.tar.gz
 tar -xvf .setup/sc64-deployer-linux-v2.20.2.tar.gz -C sc64-deployer
 chmod +x sc64-deployer/sc64deployer
+
